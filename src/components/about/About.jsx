21d4f7b6f7aa6a627f16d42013c7307816ff8919
Olivia Cesta <oliviacesta@gmail.com>
2023-01-06 02:58:09 -0500

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './about.css' // make sure to import css file for 'about' section
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './contact.css' // import css file
+
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
