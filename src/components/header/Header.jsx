a9258e3742ace09524cbeebd91011ac78c5678bc
oliviacesta <oliviacesta@gmail.com>
2023-05-10 11:13:14 -0400

    Added image, social icons, justified header text/buttons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.jpg'
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -8,8 +10,15 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1> Olivia Cesta</h1>
-        <h5 className="text-light">Aspiring Fullstack Developer</h5>
+        <h5 className="text-light">Systems Design Engineering Student</h5>
         <CTA />
+        <HeaderSocial />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
