e0b6f3d5f539a3735bd1f3e03f9faa818df4e5b2
Olivia Cesta <ocesta@uwaterloo.ca>
2024-01-15 14:18:16 -0500

    Added a template for services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
@@ -12,7 +13,101 @@ const Services = () => {
           <div className="service__head">
             <h3>UI/UX Design</h3>
           </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph 2</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph 3</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph 4</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph 5</p>
+            </li>
+          </ul>
         </article>
+        {/* END OF UI/UX  - Designathon Info Here */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph 2</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph 3</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph 4</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph 5</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF WEB DEVELOPMENT */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph 2</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph 3</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph 4</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p> Paragraph 5</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF CONTENT CREATION */}
       </div>
     </section>
   )
